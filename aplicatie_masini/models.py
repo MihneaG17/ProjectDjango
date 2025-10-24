55709ad2e7a36817ab100772d6769a1c7f9a3b85
MihneaG17 <mihnea298@gmail.com>
2025-10-24 16:10:41 +0300

    Added models

--- aplicatie_masini/models.py
+++ aplicatie_masini/models.py
@@ -11,35 +11,69 @@ class Locatie(models.Model):
     def __str__(self):
         return f"{self.adresa}, {self.oras}"
 
-class Masina(models.Model):
-    id_masina = models.IntegerField()
-    model = models.CharField(max_length=100)
-    an_fabricatie = models.IntegerField()
-    pret_masina = models.FloatField()
-    in_stoc = models.BooleanField()
-    id_marca = models.IntegerField()
-    data_adaugarii = models.DateField()
-    
 class Marca(models.Model):
-    id_marca = models.IntegerField()
-    nume_marca = models.CharField(max_length=255)
+    #id_marca = models.IntegerField()
+    nume_marca = models.CharField(max_length=255, unique=True)
     tara_origine = models.CharField(max_length=255)
     an_infiintare = models.IntegerField()
-
+    
+    def __str__(self):
+        return self.nume_marca
+    
+class CategorieMasina(models.Model):
+    #id_categorie = models.IntegerField()
+    nume_categorie = models.CharField(max_length=255)
+    descriere = models.TextField()
+    
+    def __str__(self):
+        return self.nume_categorie
+ 
 class Serviciu(models.Model):
-    id_serviciu = models.IntegerField()
+    #id_serviciu = models.IntegerField()
     nume_serviciu = models.CharField(max_length=255)
     pret_serviciu = models.FloatField()
-    descriere_serviciu = models.TextField()
-
-class CategorieMasina(models.Model):
-    id_categorie = models.IntegerField()
-    nume_categorie = models.CharField(max_length=255)
-    descriere_categorie = models.TextField()
-    tip_combustibil = models.CharField(max_length=255)
+    descriere_serviciu = models.TextField(null=True)
+    
+    def __str__(self):
+        return self.nume_serviciu
 
 class Accesoriu(models.Model):
-    id_accesoriu = models.IntegerField()
+    #id_accesoriu = models.IntegerField()
     nume_accesoriu = models.CharField(max_length=255)
     pret_accesoriu = models.FloatField()
     stoc_accesoriu = models.IntegerField()
+    
+    def __str__(self):
+        return self.nume_accesoriu
+    
+class Masina(models.Model):
+    
+    class TipCombustibil(models.TextChoices):
+        BENZINA = 'BENZINA', 'Benzină'
+        DIESEL = 'DIESEL', 'Diesel'
+        ELECTRIC = 'ELECTRIC', 'Electric'
+        HIBRID = 'HIBRID', 'Hibrid'
+        GPL = 'GPL', 'GPL'
+        ALTUL = 'ALTUL', 'Altul'
+        
+    #id_masina = models.IntegerField() - gestionat automat de django
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categorie = models.ForeignKey(CategorieMasina, on_delete=models.CASCADE)
+    pret_masina = models.FloatField()
+    in_stoc = models.BooleanField(default = True)
+    model = models.CharField(max_length=100)
+    an_fabricatie = models.IntegerField()
+    kilometraj = models.IntegerField(default = 0)
+    tip_combustibil = models.CharField(
+        max_length=100,
+        choices = TipCombustibil.choices,
+        default = TipCombustibil.BENZINA
+        )
+    data_adaugarii = models.DateField(auto_now_add=True)
+    
+    servicii = models.ManyToManyField(Serviciu, blank=True)
+    accesorii = models.ManyToManyField(Accesoriu, blank=True)
+    
+    def __str__(self):
+        return f"{self.marca.nume_marca} {self.model} ({self.an_fabricatie})"
+
