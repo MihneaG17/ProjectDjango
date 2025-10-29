7085476274bcd6795e2522989919ee230193fc4a
MihneaG17 <mihnea298@gmail.com>
2025-10-29 22:55:35 +0200

    Admin page modified with filters

--- aplicatie_masini/admin.py
+++ aplicatie_masini/admin.py
@@ -4,14 +4,8 @@ from .models import Locatie, Masina, Marca, CategorieMasina, Serviciu, Accesoriu
 admin.site.site_header = "Panou de Administrare Magazin de masini"
 admin.site.site_title = "Admin Site Masini"
 admin.site.index_title = "Bine ai venit în panoul de administrare"
-# Register your models here.
 
-#admin.site.register(Locatie)
-admin.site.register(Marca)
-admin.site.register(CategorieMasina)
-admin.site.register(Serviciu)
-admin.site.register(Accesoriu)
-admin.site.register(Masina)
+# Register your models here.
 
 class LocatieAdmin(admin.ModelAdmin):
     list_display = ('oras', 'judet') #afiseaza campurile in lista de obiecte
@@ -27,5 +21,50 @@ class LocatieAdmin(admin.ModelAdmin):
         }),
     )
 
+class MasinaAdmin(admin.ModelAdmin):
+    ordering = ['pret_masina', '-kilometraj']
+    search_fields=('model','an_fabricatie')
+    list_display = ('model', 'marca', 'categorie')
+    list_filter=['categorie', 'an_fabricatie', 'marca']
+    list_per_page=5
+    fieldsets= (
+        ('Informatii Generale', {
+            'fields': ('marca', 'categorie', 'model')
+        }),
+        ('Informatii Specifice', {
+            'fields': ('kilometraj', 'tip_combustibil','pret_masina', 'an_fabricatie', 'in_stoc') 
+        }),
+        ('Suplimentare', {
+            'fields': ('servicii', 'accesorii',),
+            'classes': ('collapse',),
+        }),
+    )
 
+class MarcaAdmin(admin.ModelAdmin):
+    ordering = ['an_infiintare']
+    search_fields=('nume_marca','tara_origine')
+    list_filter=['tara_origine',]
+    list_display = ('nume_marca', 'tara_origine')
+
+class CategorieMasinaAdmin(admin.ModelAdmin):
+    ordering = ['nume_categorie']
+    search_fields=('nume_categorie', 'descriere')
+    list_filter=['nume_categorie',]
+    list_display = ('nume_categorie',)
+
+class ServiciuAdmin(admin.ModelAdmin):
+    ordering = ['-pret_serviciu']
+    search_fields=('nume_serviciu', 'descriere_serviciu')
+    list_display = ('nume_serviciu','pret_serviciu')
+
+class AccesoriuAdmin(admin.ModelAdmin):
+    ordering = ['-pret_accesoriu']
+    search_fields=('nume_accesoriu', 'pret_accesoriu')
+    list_display = ('nume_accesoriu','pret_accesoriu')
+    
 admin.site.register(Locatie, LocatieAdmin)
+admin.site.register(Marca, MarcaAdmin)
+admin.site.register(Masina, MasinaAdmin)
+admin.site.register(CategorieMasina, CategorieMasinaAdmin)
+admin.site.register(Serviciu, ServiciuAdmin)
+admin.site.register(Accesoriu, AccesoriuAdmin)
